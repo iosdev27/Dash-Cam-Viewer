767b5df0a7dd8f4963a52fcd2e699138c24c7d09
Vin Chopra <VinChopra@Vins-iMac.local>
2016-03-15 17:00:31 -0700

    Added custom tableview to events screen. Added a hamburger menu VC with gray background color.

---
+++ DC PC Viewer/HamburgerMenuViewController.h
@@ -0,0 +1,13 @@
+//
+//  HamburgerMenuViewController.h
+//  DC PC Viewer
+//
+//  Created by Vin Chopra on 3/15/16.
+//  Copyright © 2016 Vin Chopra. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface HamburgerMenuViewController : UIViewController
+
+@end
